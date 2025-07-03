706faa76c061203a990e1edb52d4e11d56c905fa
thurayaalz <thuraya0alz@gmail.com>
2025-07-04 02:45:11 +0300

    choices functions

---
+++ rps.js
@@ -0,0 +1,18 @@
+
+function getComChoice(){
+    return Math.floor(Math.random()*3); 
+};
+
+function getHuChoice(){
+    return prompt("whats your choice?");
+}
+
+const comChoice = getComChoice();
+const huChoice = getHuChoice();
+function playRound(comChoice , huChoice){
+
+}
+console.log(huChoice);
+console.log(comChoice);
+let Hscore = 0;
+let Cscore = 0;
